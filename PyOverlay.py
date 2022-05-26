1d00e5cb04d59eb7ad11c46bb3c4e13c27c475f6
Kopamed <kopamedve@gmail.com>
2022-05-27 01:14:55 +0200

    Update PyOverlay.py

--- PyOverlay.py
+++ PyOverlay.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 # VERSION variable MUST be on the 4th line
-VERSION = 1.1
+VERSION = 1.0
 
 try:  # installing and importing all the needed packages
     import json
